96773f57fa8b55b10471833aba5e7967e859a7f3
Xein <72787884+ynks@users.noreply.github.com>
2025-11-18 08:51:28 +0100

    Initial commit

---
+++ tests/PluginBasics.cpp
@@ -0,0 +1,30 @@
+#include "helpers/test_helpers.h"
+#include <PluginProcessor.h>
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_string.hpp>
+
+TEST_CASE ("one is equal to one", "[dummy]")
+{
+    REQUIRE (1 == 1);
+}
+
+TEST_CASE ("Plugin instance", "[instance]")
+{
+    PluginProcessor testPlugin;
+
+    SECTION ("name")
+    {
+        CHECK_THAT (testPlugin.getName().toStdString(),
+            Catch::Matchers::Equals ("Pamplejuce Demo"));
+    }
+}
+
+
+#ifdef PAMPLEJUCE_IPP
+    #include <ipp.h>
+
+TEST_CASE ("IPP version", "[ipp]")
+{
+    CHECK_THAT (ippsGetLibVersion()->Version, Catch::Matchers::Equals ("2022.2.0 (r0x42db1a66)"));
+}
+#endif
